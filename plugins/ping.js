31511740e27f44df7ab8dc9e3305722c89905465
Dinidusilva66 <118092626+Dinidusilva66@users.noreply.github.com>
2025-01-19 13:41:19 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -17,7 +17,7 @@ cmd({
 async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 var inital = new Date().getTime();
-let ping = await conn.sendMessage(from , { text: '*ᴍᴀʟᴠɪɴ xᴍᴅ sᴘᴇᴇᴅ........*'  }, { quoted: mek } )
+let ping = await conn.sendMessage(from , { text: '*පොඩ්ඩක් ඉදාම් බොක්ක...*'  }, { quoted: mek } )
 var final = new Date().getTime();
 await conn.sendMessage(from, { delete: ping.key })
 return await conn.sendMessage(from , { text: '*☄️Pong*\n *' + (final - inital) + ' ms* '  }, { quoted: mek } )
